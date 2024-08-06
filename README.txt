c14b1406269f105aeffc0df5052be4d9f0ac146d
Facundo Bocchetto <BocchettoNicolas@hotmail.com>
2024-08-06 19:41:09 -0300

    Trabajo Terminado

---
+++ README.txt
@@ -0,0 +1,6 @@
+En mi segunda entrega me base en lo que va a ser mi proyecto final.
+
+Hice un menu digital el cual recibe los productos por 3 arrays con sus objetos, tambien cuenta con un menu desplegable con un Evento, el cual-
+me permite tener un escuchador para que cuando el usuario haga click se active.
+
+Por ultimo, tenemos un btn el cual me permite agregar productos al carrito y eliminar los productos por sus "ID".
--- main.js
+++ main.js
@@ -1,46 +1,46 @@
+//CONTENEDOR DE LOS PRODUCTOS DONDE SE PUSHEAN
+const containerProductos = document.querySelector("#productos");
 
-/* BOTON PARA ABRIR EL MENU DEL CARRITO */
-document.addEventListener('DOMContentLoaded', function(){
-    let btnCarrito = document.getElementById('btn-carrito');
-    let btnClosed = document.getElementById('btn-closed');
-    let menuToggle = document.getElementById('menu-carrito');
-    
+//ARRAY DE LOS PRODUCTOS CON SUS OBJETOS
+const contenedorProductosBurguers = document.querySelector('#contenedor-burguers');
+const contenedorProductosBebidas = document.querySelector('#contenedor-bebidas');
+const contenedorProductosFrito = document.querySelector('#contenedor-frito');
 
-    btnCarrito.addEventListener('click', function(){
-        menuToggle.classList.toggle('active');
-    });
-
-    btnClosed.addEventListener('click', function(){
-        menuToggle.classList.toggle('active')
-    });
-});
+//carrito
+const carritoProductos = document.querySelector("#contenedor-productos");
+const carritoVacio = document.querySelector("#carrito-vacio");
 
 
-
+//ARRAY DONDE SE ALMACENAN LOS PRODUCTOS QUE VAMOS AÑADIENDO AL CARRITO
 const carrito =[
 
 ]
 
+//ARRAY DE LOS PRODUCTOS CON SUS OBJETOS
 const burguers = [
     {
+        id: "1",
         nombre: "American",
         descripcion: "Medallon 120gr (blend secreto), cheddar x4, bacon ahumado, alioli de berenjena ahumada, pepinillos.",
         precio: "$10900",
         imagen: "IMAGENES/american.png",
     },
     {
+        id: "2",
         nombre: "Cheese Bacon",
         descripcion: "Medallon 120gr (blend secreto), cheddar x4, bacon ahumado",
         precio: "$10700",
         imagen:"IMAGENES/bacon-chese.jpg",
     },
     {
+        id: "3",
         nombre: "Candy",
         descripcion: "Medallon 120gr (blend secreto), cheddar x4, bacon ahumado, cebolla caramelizada",
         precio: "$10700",
         imagen:"IMAGENES/candy.png",
     },
     {
+        id: "4",
         nombre: "Crispy Onion",
         descripcion: "Medallon 120gr (blend secreto), cheddar x4, bacon ahumado, ali oli, mil islas, cebolla crispy, pepinillos (opcional)",
         precio: "$10900",
@@ -51,21 +51,25 @@ const burguers = [
 
 const bebidas = [
     {
+        id: "5",
         nombre: "Coca Cola",
         precio: "$1000",
         imagen:"IMAGENES/cocabotella.jpg",
     },
     {
+        id: "6",
         nombre: "Agua",
         precio: "$900",
         imagen:"IMAGENES/agua.jpg",
     },
     {
+        id: "7",
         nombre: "Schweppes pomelo",
         precio: "$1000",
         imagen:"IMAGENES/swet.jpg",
     },
     {
+        id: "8",
         nombre: "Aquarius Pera",
         precio: "$1000",
         imagen:"IMAGENES/aquarius.webp",
@@ -74,24 +78,28 @@ const bebidas = [
 
 const frituras = [
     {
+        id: "9",
         nombre: "Papas Fritas",
         descripcion: "",
         precio: "$2000",
         imagen:"IMAGENES/papaFritas.webp",
     },
     {
+        id: "10",
         nombre: "Papas con Cheddar y Bacon",
         descripcion: "",
         precio: "$3500",
         imagen:"IMAGENES/papasFritas.png",
     },
     {
+        id: "11",
         nombre: "Nuggets",
         descripcion: "",
         precio: "$5000",
         imagen:"IMAGENES/nuggets.jpg",
     },
     {
+        id: "12",
         nombre: "Tequeños",
         descripcion: "",
         precio: "$5000",
@@ -99,26 +107,7 @@ const frituras = [
     }
 ]
 
-const containerProductos = document.querySelector("#productos"); //ESTE NO LO USO
-const carritoVacio = document.querySelector("#carrito-vacio");
-const contenedorProductosBurguers = document.querySelector('#contenedor-burguers');
-const contenedorProductosBebidas = document.querySelector('#contenedor-bebidas');
-const contenedorProductosFrito = document.querySelector('#contenedor-frito');
-
-//carrito
-
-const carritoTotal = document.querySelector("#carrito-total");
-
-/*
-<div class="contenedor-producto">
-    <img src="IMAGENES/american.png" alt="">
-    <div class="detalles-productos" ">
-        <h4 class="nombre-producto"></h4>
-        <p class="descripcion-producto"></p>
-        <p class="precio-producto"></p>
-        <button class="carrito-add"><i class='bx bx-cart-add'></i></button>
-    </div>
-</div>*/
+//HAMBURGUESAS
 burguers.forEach((burguer) =>{
     let div = document.createElement("div");
     div.classList.add("burguers");
@@ -144,7 +133,7 @@ burguers.forEach((burguer) =>{
 })
 
 
-
+//BEBIDAS
 bebidas.forEach((bebida) =>{
     let div = document.createElement("div");
     div.classList.add("bebidas");
@@ -161,12 +150,14 @@ bebidas.forEach((bebida) =>{
 
     let button = div.querySelector(".carrito-add");
     button.addEventListener("click", ()=>{
-        console.log(bebida)
+        agregarAlCarrito(bebida)
     });
 
     contenedorProductosBebidas.append(div);
 })
 
+
+//FRITURAS
 frituras.forEach((frito) =>{
     let div = document.createElement("div");
     div.classList.add("frituras");
@@ -183,14 +174,77 @@ frituras.forEach((frito) =>{
 
     let button = div.querySelector(".carrito-add");
     button.addEventListener("click", ()=>{
-        console.log(frito)
+        agregarAlCarrito(frito)
     });
-
 
     contenedorProductosFrito.append(div);
 })
 
-const agregarAlCarrito = (burguers) =>{
-    carrito.push(burguers);
-    console.log(carrito);
+
+
+
+//AÑADE EL ELEMENTO AL CARRITO Y LUEGO ACTUALIZA LA VISTA DEL CARRITO PARA PODER VER EL PRODUCTO DENTRO DEL CARRITO
+const agregarAlCarrito = (productos) =>{
+    carrito.push(productos);
+    estadoCarrito();
+    
 }
+
+//FUNCION LA CUAL CONTROLA EL ESTADO DEL CARRITO, SI NO HAY ELEMENTOS DENTRO MUESTRA UN MENSAJE.
+//EN CASO DE QUE HAYAN ELEMENTOS DENTRO, MUESTRA LOS PRODUCTOS.
+function estadoCarrito(){
+    if(carrito.length === 0){
+        carritoVacio.classList.remove("d-none");
+        carritoProductos.classList.add("d-none");
+    } else{
+        carritoVacio.classList.add("d-none");
+        carritoProductos.classList.remove("d-none");
+
+        carritoProductos.innerHTML = "";
+        carrito.forEach((productos) =>{
+
+            let div = document.createElement("div");
+            div.classList.add("carrito-productos");
+            div.innerHTML = `
+            <div class="carrito-productos">       
+                <img src="${productos.imagen}" alt="">
+                <h4 class="nombre-producto">${productos.nombre}</h4>
+                <p class="precio-producto">${productos.precio}</p>
+                <button class="btn-remove">✖️</button>
+            </div>
+            `
+
+            let btnRemove = div.querySelector(".btn-remove");
+            btnRemove.addEventListener("click", () =>{
+                eliminarElementoDelCarrito(productos);
+            })
+            carritoProductos.append(div);
+        })
+    }
+}
+
+
+//LO QUE HACE ESTA FUNCION ES BUSCAR EL ELEMENTO EN EL CARRITO CON SU ID, LO BORRA Y LUEGO ACTUALIZA EL CARRITO
+function eliminarElementoDelCarrito(productos){
+    const indice = carrito.findIndex((item) => item.id === productos.id);
+    carrito.splice(indice, 1);
+    estadoCarrito();
+}
+
+
+
+/* BOTON PARA ABRIR Y CERRAR EL MENU DEL CARRITO */
+document.addEventListener('DOMContentLoaded', function(){
+    let btnCarrito = document.getElementById('btn-carrito');
+    let btnClosed = document.getElementById('btn-closed');
+    let menuToggle = document.getElementById('menu-carrito');
+    
+
+    btnCarrito.addEventListener('click', function(){
+        menuToggle.classList.toggle('active');
+    });
+
+    btnClosed.addEventListener('click', function(){
+        menuToggle.classList.toggle('active')
+    });
+});
